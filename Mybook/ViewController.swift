6cff5a4a35db768d214c7ff75506d1692ca92425
jbirdx <keixxxxxxxx@gmail.com>
2018-06-10 03:32:53 +0900

    Initial Commit

---
+++ Mybook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mybook
+//
+//  Created by 伊藤慶 on 2018/06/10.
+//  Copyright © 2018年 伊藤慶. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
